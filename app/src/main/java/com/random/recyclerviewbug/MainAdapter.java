288e13e3ea6c82ccecf07f814be006964f8925f7
Rait Maltsaar <rait@pairby.com>
2016-09-06 09:45:19 +0300

    Adapter uses DiffUtil to find the differences between the old and new state of the dataset

--- app/src/main/java/com/random/recyclerviewbug/MainAdapter.java
+++ app/src/main/java/com/random/recyclerviewbug/MainAdapter.java
@@ -1,10 +1,12 @@
 package com.random.recyclerviewbug;
 
 import android.content.Context;
+import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainAdapter extends RecyclerView.Adapter<MainViewHolder> {
@@ -12,7 +14,7 @@ public class MainAdapter extends RecyclerView.Adapter<MainViewHolder> {
 	private List<MainModel> dataset;
 
 	public MainAdapter(List<MainModel> dataset) {
-		this.dataset = dataset;
+		this.dataset = new ArrayList<>(dataset);
 		setHasStableIds(true);
 	}
 
@@ -41,7 +43,36 @@ public class MainAdapter extends RecyclerView.Adapter<MainViewHolder> {
 	}
 
 	public void datasetChanged(List<MainModel> dataset) {
+		DiffUtil.DiffResult diffResult = calculateDiffResult(dataset);
 		this.dataset = dataset;
-		notifyItemInserted(0);
+		diffResult.dispatchUpdatesTo(this);
+	}
+
+	private DiffUtil.DiffResult calculateDiffResult(final List<MainModel> dataset) {
+		return DiffUtil.calculateDiff(new DiffUtil.Callback() {
+			@Override
+			public int getOldListSize() {
+				return MainAdapter.this.dataset.size();
+			}
+
+			@Override
+			public int getNewListSize() {
+				return dataset.size();
+			}
+
+			@Override
+			public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+				MainModel oldItem = MainAdapter.this.dataset.get(oldItemPosition);
+				MainModel newItem = dataset.get(newItemPosition);
+				return oldItem.id == newItem.id;
+			}
+
+			@Override
+			public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+				MainModel oldItem = MainAdapter.this.dataset.get(oldItemPosition);
+				MainModel newItem = dataset.get(newItemPosition);
+				return oldItem.text.equals(newItem.text);
+			}
+		});
 	}
 }
